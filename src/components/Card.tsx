bd814dc521cd5310d66dc4fef27bf9b6ed939dab
masudrahman-dev <masudrahman.dev@gmail>
2023-08-01 07:49:33 +0600

    test

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface CardProps {
   url: string;
 }
 
 const Card: React.FC<CardProps> = ({ url }) => {
+  const [lineEffect, setLineEffect] = useState();
+
   const textDesign: React.CSSProperties = {
     writingMode: "vertical-rl",
     transform: "rotate(180deg)",
   };
 
-
   return (
-    <div className="flex justify-center gap-3  border-gray-500 hover:scale-105 transition-all delay-75">
+    <div
+      className="flex justify-center gap-3  border-gray-500
+      hover:scale-105 transition-all delay-75"
+    >
       <div style={textDesign}>
         <h1 className="md:text-[43px] text-base font-bold">HEADPHONE</h1>
-        <p className="md:text-[25px] text-[12px] font-semibold">Best Budget Headset...</p>
+        <p className="md:text-[25px] text-[12px] font-semibold">
+          Best Budget Headset...
+        </p>
       </div>
       <div className="flex">
         <img
-          className=" "
           style={{ boxShadow: "rgba(0, 0, 0, 0.67) 3px 5px 20px 1px" }}
           src={url}
           alt=""
@@ -30,4 +35,3 @@ const Card: React.FC<CardProps> = ({ url }) => {
 };
 
 export default Card;
-
